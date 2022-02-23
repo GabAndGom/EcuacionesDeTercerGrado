4a4115d5de88a986c5f7b2a9a3fc42f90251aaa4
Gabriel Gomez <andresgabrielgc@gmail.com>
2022-02-23 16:21:33 -0400

    correciones finales

--- Ecuaciones de tercer grado.cpp
+++ Ecuaciones de tercer grado.cpp
@@ -9,162 +9,110 @@ la cual se resolvera por la formula general*/
 using namespace std;
 int main ()
 {
-    double A,AA,B,C,D,RESP=0,p,q,d,x1,x2,x3,aux1,aux2,r2d,auxq;//p, q y d valores para cardamo.
-    while (RESP !=1)
+    float p,q,delta;         //p, q y d valores para cardamo.
+    float A,B,C,D,A1,B1,C1,D1,a,b,c;
+    double aux01,aux02,aux03;
+    float x1,x2,x3,xreal,ximag;
+    cout<<"Introduzca los coeficionetes de la ecuacion de tercer grado de la forma Ax^3+Bx^2+Cx+D=0"<<endl;
+    cout<<"Digite A: ";
+    cin>>A;
+    cout<<"Digite B: ";
+    cin>>B;
+    cout<<"Digite C: ";
+    cin>>C;
+    cout<<"Digite D: ";
+    cin>>D;
+    cout<<"La ecuacion es: "<<A<<"x^3+"<<B<<"x^2+"<<C<<"x+"<<D<<"=0"<<endl;
+    if (A==1)
     {
-        RESP=0;
-        cout<<"Introduzca los coeficionetes de la ecuacion de tercer grado de la forma Ax^3+Bx^2+Cx+D=0"<<endl;
-        cout<<"Digite A: ";
-        cin>>A;
-        cout<<"Digite B: ";
-        cin>>B;
-        cout<<"Digite C: ";
-        cin>>C;
-        cout<<"Digite D: ";
-        cin>>D;
-        cout<<"La ecuacion es: "<<A<<"x^3+"<<B<<"x^2+"<<C<<"x+"<<D<<"=0?"<<endl;
-        cout<<"Digite con 1 para continuar o con cualquier numero para volver a digitar los coedifientes"<<endl;
-        cin>>RESP;
-
-    }
-    if (A==0)
-    {
-        cout<<"es una ecuacion cuadratica de forma Bx^2+CxD=0 "<<endl;
-        float res1=0,res2=0;
-        float xreal,ximag;
-        aux1=(pow(C,2)-(4*B*D));
-        if (aux1<0)
-        {
-            cout<<"la ecuacion no tiene solucion en los reales "<<endl;
-            xreal=-C/(2*B);
-            ximag=sqrt(-aux1)/(2*B);
-            cout<<"Las soluciones imaginarias son: "<<endl;
-            cout<<xreal<<"+"<<ximag<<"i "<<endl;
-            cout<<xreal<<"-"<<ximag<<"i "<<endl;
-        }
-        else
-        {
-            if (B==0)
-            {
-               cout<<"la ecuacion no es de segundo grado ,la solucion es: "<<D/C;
-            }
-            else
-            {
-               res1=(-C-(sqrt(aux1)))/(2*B);
-               res2=(-C+(sqrt(aux1)))/(2*B);
-               if (res1==res2)
-               {
-                  cout<<"la solucion para la ecuacion es: "<<res1<<endl;
-               }
-                if ((aux1<0)||(B==0))
-                {
-                   cout<<".";
-                }
-                if (res1!=res2) 
-                {
-                   cout<<"las soluciones para la ecuacion son: "<<res1<<" y "<<res2<<endl;
-                }
-        
-            }
-        }
-
-    }
-    AA=A;
-    if (A!=1)
-    {
-        A=A/AA;
-        B=B/AA;
-        C=C/AA;
-        D=D/AA;
-    }
-    p=((3*C)-pow(B,2))/3;
-    q= ((2*(pow(B,3)))-(9*B*C)+(27*D))/27;
-    d=(pow((q/2),2)+pow((p/3),3));
-    r2d=sqrt(d);
-    auxq=(-q/2);
-    aux1= auxq+r2d;
-    aux2= auxq-r2d;
-    if (aux1<0)
-    {
-        aux1=aux1*-1;
-        aux1=-pow(aux1,(1.0/3.0));
+        a=B;
+        b=C;
+        c=D;
     }
     else 
     {
-        aux1=pow(aux1,(1.0/3.0));
+        A1=A/A;
+        B1=B/A;
+        C1=C/A;
+        D1=D/A;
+        a=B1;
+        b=C1;
+        c=D1;
     }
-    if (aux2<0)
+    p=((3*b)-(a*a))/3;
+    q=((2*a*a*a)-(9*a*b)+(27*c))/27;
+    delta=(pow((q/2),2))+pow((p/3),3);
+    if (delta<0)
     {
-        aux2=aux2*-1;
-        aux2=-pow(aux2,(1.0/3.0));
+       aux01=((-q/2)+sqrt(-delta));
+       aux02=((-q/2)-sqrt(-delta));
+       aux01=cbrt(aux01);
+       aux02=cbrt(aux02);
+       cout<<"p="<<p<<endl;
+       cout<<"q="<<q<<endl;
+       cout<<"delta="<<delta<<endl;
+
     }
     else
     {
-        aux2=pow(aux2,(1.0/3.0));
+       aux01=((-q/2)+sqrt(delta));
+       aux02=((-q/2)-sqrt(delta));
+       aux01=cbrt(aux01);
+       aux02=cbrt(aux02);
+       cout<<"p="<<p<<endl;
+       cout<<"q="<<q<<endl;
+       cout<<"delta="<<delta<<endl;
     }
 
-    x1=aux1+aux2-(B/3);
-    cout<<"x1= "<<x1<<endl;
-    /*Ahora debemos aplicar rufini para debelar una ecuacion cuadratica*/
-
-
-
-    float a,b,c,e;//e es el reemplazo de D porque ya se uso d anteriormente.
-    float aa,bb,cc,dd;
-    a=A*AA;
-    b=B*AA;
-    c=C*AA;
-    e=D*AA;
-
-    aa=a;
-    bb=(aa*x1)+b;
-    cc=(bb*x1)+c;
-    dd=(cc*x1)+e;
+    x1=aux01+aux02-(a/3);           //esta es la primera raiz 
+                                   //ahora debemos desarollar por rufini con la raiz encontrada para obtener una ecuacion cuadratica.
     
-    float xreal1,ximag2,aux11,res1,res2;
-   
-    aux11=(pow(bb,2)-(4*aa*cc));
-    if (aux11<0)
+    B=((A*x1)+B);
+    C=(B*x1)+C;
+    aux03=(B*B)-(4*A*C);
+    cout<<x1<<endl;
+    cout<<"A="<<A<<endl;
+    cout<<"B="<<B<<endl;
+    cout<<"C"<<C<<endl;
+    if(A==0)
     {
-        cout<<"la ecuacion no tiene solucion en los reales "<<endl;
-        xreal1=-bb/(2*aa);
-        ximag2=sqrt(-aux11)/(2*aa);
-        cout<<"Las soluciones imaginarias son: "<<endl;
-        cout<<xreal1<<"+"<<ximag2<<"i "<<endl;
-        cout<<xreal1<<"-"<<ximag2<<"i "<<endl;
+        cout<<"la ecuacion no es de tercer grado ,la solucion es: "<<C/B;
     }
     else
     {
-        if (aa==0)
-       {
-        cout<<"la ecuacion no es de segundo grado ,la solucion es: "<<cc/bb;
-       }
-        else
-       {
-              res1=(-bb-(sqrt(aux11)))/(2*aa);
-            res2=(-bb+(sqrt(aux11)))/(2*aa);
-            if (res1==res2)
+        if (aux03<0)
+        {
+        cout<<"la solucion en los reales es: "<<x1<<endl;
+        xreal=-B/(2*A);
+        ximag=sqrt(-aux03)/(2*A);
+        cout<<"Las soluciones imaginarias son: "<<endl;
+        cout<<xreal<<"+"<<ximag<<"i "<<endl;
+        cout<<xreal<<"-"<<ximag<<"i "<<endl;
+        }
+        else 
+        {
+            if (aux03==0)
             {
-                cout<<"la solucion para la ecuacion es: "<<res1<<endl;
+                cout<<"la soluciones para la ecuacion son: "<<x1<<" y "<<-B/(2*A)<<endl;
             }
-            if ((aux1<0)||(aa==0))
+            else 
             {
-                cout<<".";
+                x2=(-B-(sqrt(aux03)))/(2*A);
+                x3=(-B+(sqrt(aux03)))/(2*A);
+                if (x2==x3)
+                {
+                  cout<<"la solucion para la ecuacion es: "<<x1<<"y "<<x2<<endl;
+                }
+                else 
+                {
+                    cout<<"las soluciones para la ecuacion son: "<<x1<<" y "<<x2<<" y "<<x3<<endl;
+                }
             }
-            if (res1!=res2) 
-            {
-             cout<<"las soluciones para la ecuacion son: "<<res1<<" y "<<res2<<endl;
-            }
-        
         }
     }
-    system("pause");
+
+   system("pause");
     
-
-
-
-    
-
 
 }
 
